a6f3d901b8ceee4f636ebe6b33c6b5ed3f6f9edb
abenetka <ali.benetka@gmail.com>
2019-03-07 15:11:00 -0500

    Adds bubbleSort algorithm

---
+++ bubbleSort.js
@@ -0,0 +1,17 @@
+function swap(array, i, j,) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function bubbleSort(array) {
+  for(var i = 0; i < array.length; i++) {
+    for(var j = 1; j < array.length; j++) {
+      if(array[j - 1] > array [j]) {
+        swap(array, j - 1, j);
+      }
+    }
+  }
+  return array;
+}
+module.exports = bubbleSort
